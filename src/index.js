54b0cef630fa47dd1947258cdde9033d0d8f6281
Ismael Navarro <ismanapa@gmail.com>
2018-08-05 23:48:59 +0200

    Added terrain

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -29,9 +36,18 @@ function preload () {
     );
 }
 
+var platforms;
+
 function create () {
     this.add.image(400, 300, assets.sky);
-    this.add.image(400, 300, assets.dude);
+    
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 560, assets.ground).setScale(2).refreshBody();
+
+    platforms.create(600, 400, assets.ground);
+    platforms.create(50, 250, assets.ground);
+    platforms.create(750, 220, assets.ground);
 }
 
 function update () {
